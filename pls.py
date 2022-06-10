e09e5354f22db0af35d6267df660572fc61cd06b
Bjorn Klaassen <1039715@hr.nl>
2022-06-10 14:03:36 +0200

    catalo + book and bookitem

--- pls.py
+++ pls.py
@@ -1,3 +1,6 @@
+from typing import ItemsView
+
+
 class Person:
    def __init__(self,username, password):
      self.username = username
@@ -10,6 +13,51 @@ class Member(Person):
 class Admin(Person):
    def __init__(self):
     Person.__init__(self,"admin","admin123")
+
+
+class catalog:
+    def __init__ (self,bookList):
+        self.bookList = bookList
+    
+    def addBook(self,bookItem):
+        self.bookList += bookItem
+
+    def searchBook(self,searchTerm):
+        ansList = []
+        for book in self.bookList:
+            if searchTerm == book.title:
+                return "Title: %s, Author: %s"
+            if searchTerm == book.author:
+                return book
+        return "Book not found"
+    
+    def addBooks(self):
+        pass
+
+    def viewbooks(self):
+        pass
+
+class Book:
+    def __init__(self,auhtor,country,imageLink,language,link,pages,title,ISBN,year):
+        self.author = author
+        self.country = country
+        self.imageLink = imageLink
+        self.language = language
+        self.link = link
+        self.pages = pages
+        self.title = title
+        self.ISBN = ISBN
+        self.year = year
+    
+class BookItem(book)
+
+    def __init__(self,amount):
+        Book.__init__(self)
+        amount = 3
+    
+    
+
+
 
 
 x = Admin()
